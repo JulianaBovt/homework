78706a2465dc99d86caff0ca096b7e91b7632477
Juliana <julianabovt@gmail.com>
2025-12-09 02:43:51 +0700

    Update main.js

--- верстка финального макета модуль 2/js/main.js
+++ верстка финального макета модуль 2/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     document.addEventListener('click', burgerInit)
 
     function burgerInit(e) {
@@ -10,9 +10,31 @@
         if (document.documentElement.clientWidth > 900) return
 
         if (!document.body.classList.contains('body--opened-menu')) {
-            document.body.classList.add('body--opened-menu') 
+            document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
         }
     }
-})()
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector ('.about__img-button')
+
+    modalButton.addEventListener('click', openModal) 
+    modal.addEventListener('click', closeModal)
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+        
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+}) ()
+
